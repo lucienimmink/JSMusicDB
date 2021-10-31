77a53a47e703d3f6e91813b2d6d41bf25c063002
Lucien Immink <lucien@arielext.org>
2021-10-31 15:55:33 +0100

    pin @thebespokepixel/es-tinycolor to 2.1.1 for now

--- src/utils/colour.ts
+++ src/utils/colour.ts
@@ -3,7 +3,7 @@ declare const window: any;
 //@ts-ignore
 import { tinycolor } from '@thebespokepixel/es-tinycolor';
 //@ts-ignore
-import FastAverageColor from 'fast-average-color/dist/index.es6';
+import FastAverageColor from 'fast-average-color';
 import { getSettingByName, setSetting } from './settings';
 
 export const ACCENT_COLOR = 'accent-color';
