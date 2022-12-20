e3164a1f4bee173621f545e2304f4d42ccea151a
Lucien Immink <lucien@arielext.org>
2022-12-20 12:02:58 +0100

    Use Vite 4

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite';
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import loadVersion from 'vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     entry: 'index.html',
+    emptyOutDir: true,
   },
   plugins: [
     loadVersion.default(),
-    chunkSplitPlugin({
-      strategy: 'default',
-      customSplitting: {
-        vendor: [/node_modules/],
-        utils: [
-          /src\/utils/,
-          /src\/components\/last-fm/,
-          /src\/components\/mp3stream/,
-        ],
-        styles: [/src\/styles/],
-      },
-    }),
   ],
 });
