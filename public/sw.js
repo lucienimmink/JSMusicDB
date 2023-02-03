29eeb92d70d5e56cadc7bd276f75a9df5daa3f37
Lucien Immink <lucien@arielext.org>
2023-02-03 15:28:28 +0100

    simplify SW

--- public/sw.js
+++ public/sw.js
@@ -5,37 +5,33 @@ self.addEventListener('activate', event => {
 
 const CACHE_NAME = 'v1';
 
+const NON_CACHEABLE = [
+  '/listen',
+  '/rescan',
+  '/version',
+  '/public-key',
+  '/stream',
+  'file://',
+  '//localhost',
+  '//ws.audioscrobbler.com',
+  '#no-sw-cache',
+  '?ts='
+];
+const CACHE_AND_UPDATE = [/\/node-music\.json$/];
+
 self.addEventListener('fetch', function (event) {
-  // SKIP cachecheck if it's the streaming path
   try {
-    if (
-      event.request.url.indexOf('/listen') !== -1 ||
-      event.request.url.indexOf('/rescan') !== -1 ||
-      event.request.url.indexOf('/version') !== -1 ||
-      event.request.url.indexOf('/public-key') !== -1 ||
-      event.request.url.indexOf('/stream') !== -1 ||
-      event.request.url.indexOf('file://') !== -1 ||
-      event.request.url.indexOf('//localhost') !== -1 ||
-      event.request.url.indexOf('//ws.audioscrobbler.com') !== -1 ||
-      event.request.url.indexOf('#no-sw-cache') !== -1
-    ) {
+    if (NON_CACHEABLE.some(url => event.request.url.indexOf(url) !== -1)) {
       // nothing to see here, carry on
-    } else if (event.request.url.indexOf('music.json') !== -1 && event.request.url.indexOf('?ts=') === -1) {
-      // send back from the cache but always update the cache with the networks version.
+    } else if (CACHE_AND_UPDATE.some(url => event.request.url.match(url))) {
       event.respondWith(fromCache(event.request));
       event.waitUntil(update(event.request).then(refresh));
     } else {
       event.respondWith(
         caches.match(event.request).then(function (response) {
-          // Cache hit - return response
           if (response) {
             return response;
           }
-
-          // IMPORTANT: Clone the request. A request is a stream and
-          // can only be consumed once. Since we are consuming this
-          // once by cache and once by the browser for fetch, we need
-          // to clone the response.
           var fetchRequest = event.request.clone();
           if (
             fetchRequest.cache === 'only-if-cached' &&
@@ -44,17 +40,11 @@ self.addEventListener('fetch', function (event) {
             return;
           }
           return fetch(fetchRequest).then(function (response) {
-            // Check if we received a valid response
             if (!response) {
               return response;
             }
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
             var responseToCache = response.clone();
-            if (responseToCache.status === 200 || responseToCache.type === 'opaque') {
-              // only cache if the response is ok
+            if (fetchRequest.method === 'GET' && (responseToCache.status === 200 || responseToCache.type === 'opaque')) {
               caches.open(CACHE_NAME).then(function (cache) {
                 cache.put(event.request, responseToCache);
               });
