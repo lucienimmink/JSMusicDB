b0b42d7895103e8d1fcc9af1d8463158eff062e3
Lucien Immink <lucien@arielext.org>
2023-05-12 23:13:13 +0200

    Update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-// curl -H "Content-Type: application/zip" -H "Authorization: Bearer zFkWLDLJ3AI4Wizd2nn-4Uuonm16iilMIZSkq_gLxtU" --data-binary "@dist.zip" https://api.netlify.com/api/v1/sites/65dee498-23a1-43a7-8686-f350ae7b4b9a/deploys
 import * as dotenv from 'dotenv';
 import fetch, { fileFromSync } from 'node-fetch';
 dotenv.config();
