6c719668e39cde6938c7624aa85104ee52377151
Lucien Immink <lucien@arielext.org>
2024-01-05 12:00:01 +0100

    Refactor handleScroll function to improve scrolling behavior

--- src/utils/virtual-scroll.ts
+++ src/utils/virtual-scroll.ts
@@ -1,11 +1,7 @@
 export const handleScroll = (scroller: any, index = 0) => {
   const offsetted = index === 0 ? 0 : index - 1;
-  scroller?.scrollToIndex(offsetted, 'start');
-  setTimeout(() => {
-    if (offsetted !== 0) {
-      scroller?.scrollToIndex(offsetted, 'start');
-    } else {
-      document.querySelector('html')?.scrollTo({ top: 0 });
-    }
-  }, 10);
+  scroller?.element(offsetted).scrollIntoView({ block: 'start' });
+  if (offsetted === 0) {
+    document.querySelector('html')?.scrollTo({ top: 0 });
+  }
 };
