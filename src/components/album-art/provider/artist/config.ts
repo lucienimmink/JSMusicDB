f4533406f970386b2e5b06ec7428ee88f2a189a0
Lucien Immink <lucien@arielext.org>
2024-05-13 11:56:35 +0200

    Re-add fanart search

--- src/components/album-art/provider/artist/config.ts
+++ src/components/album-art/provider/artist/config.ts
@@ -1,13 +1,12 @@
 import { fetchArt as mp3stream } from '../mp3stream';
 import { fetchArt as audiodb } from './audiodb';
-// import { fetchArt as fanart } from './fanart';
+import { fetchArt as fanart } from './fanart';
 
 const config = [
-  // seems to beat mp3stream but no data coming back, investigation needed.
-  // {
-  //   provider: fanart,
-  //   key: 'mbid',
-  // },
+  {
+    provider: fanart,
+    key: 'mbid',
+  },
   {
     provider: mp3stream,
     key: 'mbid',
