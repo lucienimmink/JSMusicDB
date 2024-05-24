3904ce2d51a6bc7041ca7a878724bce3b2908186
Lucien Immink <l.immink@teamrockstars.nl>
2024-05-24 11:50:37 +0200

    Add deezer as album art provider

--- src/components/album-art/provider/album/config.ts
+++ src/components/album-art/provider/album/config.ts
@@ -1,5 +1,6 @@
 import { fetchArt as mp3stream } from './../mp3stream';
 import { fetchArt as lastfm } from './lastfm';
+import { fetchArt as deezer } from './deezer';
 import { fetchArt as coverartarchive } from './coverartarchive';
 
 const config = [
@@ -9,6 +10,9 @@ const config = [
   {
     provider: mp3stream,
     key: 'mbid',
+  },
+  {
+    provider: deezer,
   },
   {
     provider: coverartarchive,
---
+++ src/components/album-art/provider/album/deezer.ts
@@ -0,0 +1,24 @@
+import { fetchWithTimeout } from '../../../../utils/fetch';
+
+const fetchArt = async ({
+  artist,
+  album,
+}: {
+  artist: string;
+  album: string;
+}) => {
+  const response = await fetchWithTimeout(
+    `https://api.deezer.com/search/artist?q=${encodeURIComponent(artist)} - ${encodeURIComponent(album)}`,
+    { timeout: 10000 },
+  );
+  if (response.status === 200) {
+    const json = await response.json();
+    const { data } = json;
+    if (data) {
+      return data[0].picture_xl;
+    }
+  }
+  throw Error('no art found in provider deezer');
+};
+
+export { fetchArt };
