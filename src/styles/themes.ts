f2e5d3921ee96f1cec13dcae929e1d867a972944
Lucien Immink <lucien@arielext.org>
2026-04-09 19:51:26 +0200

    Refactor color variables in base.css and themes.ts
    - Added new CSS variables for letter color and text color in base.css.
    - Removed specific letter color and text color definitions from light and dark themes in themes.ts, allowing for more flexible color management.

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -6,10 +6,8 @@ export const light = css`
     --progress-background: var(--lighten30);
     --letter-background-hover: var(--darken);
     --letter-background-active: var(--darken30);
-    --letter-color: oklch(from var(--primary) calc(l + 0.8) c h);
 
     --background: white;
-    --text-color: oklch(from var(--background) calc(l - 0.99) c h);
     --background2: oklch(from var(--background) calc(l - 0.03) c h);
     --border-colour: oklch(from var(--background) calc(l - 0.15) c h);
     --background3: oklch(from var(--background) calc(l - 0.06) c h);
@@ -24,10 +22,8 @@ export const dark = css`
     --progress-background: var(--darken30);
     --letter-background-hover: var(--lighten);
     --letter-background-active: var(--lighten30);
-    --letter-color: oklch(from var(--primary) calc(l - 0.8) c h);
 
     --background: rgb(31, 31, 31);
-    --text-color: oklch(from var(--background) calc(l + 0.99) c h);
     --background2: oklch(from var(--background) calc(l + 0.03) c h);
     --border-colour: oklch(from var(--background) calc(l + 0.15) c h);
     --background3: oklch(from var(--background) calc(l + 0.06) c h);
